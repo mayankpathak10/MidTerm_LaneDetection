1c4ec9263efd6a13c402062d13eeb8256c1cb2bd
MAYANK <pathak10@umd.edu>
2018-10-07 13:36:55 -0400

    Product Backlog#7-CCD Create LaneDetector.hpp

--- include/LaneDetector.hpp
+++ include/LaneDetector.hpp
@@ -1,8 +1,57 @@
 #pragma once
 
+#include <opencv2/highgui/highgui.hpp>
 #include<iostream>
+#include <string>
+#include <vector>
+#include "opencv2/opencv.hpp"
 
-void dummy()
-{
-    std::cout << "HI" << std::endl;
+class LaneDetector {
+ private:
+ 	 cv::Mat inputImage;
+ 	 cv::Mat roiImage;
+ 	 cv::Mat grayImage;
+ 	 cv::Mat bwImage;
+ 	 cv::Mat edgedImage;
+ 	 cv::Mat threshImage;
+ 	 std::vector<int> lines;
+ 	 double rho = 1;
+ 	 double theta = 1;
+ 	 double hthreshold;
+ 	 double leftSlope;
+ 	 double rightSlope;
+ 	 double leftPoints;
+ 	 double rightPoints;
+ 	 double meanLeftSlope;
+ 	 double meanRightSlope;
+ 	 string turnPredict;
+
+ public: 
+	 cv::Mat roiMastSelection(cv::Mat inputImage) {
+
+	 }
+
+	 cv::Mat hsvThreshold(cv::Mat roiImage) {
+
+	 }
+
+	 cv::Mat edgeDetector(cv::Mat threshImage) {
+
+	 }
+
+	 std::vector houghTransform(cv::Mat edgedImage) {
+
+	 }
+
+	 double slopeCalculation(std::vector<int>) {
+
+	 }
+
+	 string predictTurn(double, double){
+
+	 } 
+
+	 void plotResults(cv::Mat, string, std::vector<int>, std::vector<int>) {
+
+	 }
 }
