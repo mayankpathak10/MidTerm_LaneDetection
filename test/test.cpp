eb13fec31e630825ebb91af08d49364559c13410
MAYANK <pathak10@umd.edu>
2018-10-09 00:38:42 -0400

    Update test.cpp . Test cases defined

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,38 @@
 #include <gtest/gtest.h>
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+/**
+ * @brief To test if the lanes are detected in the given 
+ * Frame.
+ */
+int LaneTesting(int framenumber) {
+
+
+}
+
+/**
+ *@brief Test case to test if lane is detected.
+ */
+TEST(LaneTest, lane_detected) {
+  EXPECT_EQ(testing_lanes(10), 0);
+}
+
+/**
+ *@brief Test cases to test if lane is detected and if the lane is going straight.
+ */
+TEST(LaneTest, no_turn) {
+  EXPECT_EQ(testing_lanes(600), 0);
+}
+
+/**
+ *@brief Test cases to test if lane is detected and if the lane is turning right.
+ */
+TEST(LaneTest, right_turn) {
+  EXPECT_EQ(testing_lanes(1500), 0);
+}
+
+/**
+ *@brief Test cases to test if lane is detected and if the lane is turning left.
+ */
+TEST(LaneTest, right_left) {
+  EXPECT_EQ(testing_lanes(2300), 0);
 }
