471aace573931148c01130c533c2921a6b7e847a
bhargav-umd <bhargav@umd.edu>
2018-10-13 00:07:15 -0400

    Fixed compiling Errors

---
+++ app/hsvThreshold.cpp
@@ -0,0 +1,22 @@
+#include "LaneDetector.hpp"
+
+cv::Mat hsvThreshold(cv::Mat roiImage) {
+    cv::Mat HSV;
+
+    // convert RGB roiImage to HSV
+    cv::cvtColor(roiImage, HSV, CV_BGR2HSV);
+    std::vector<cv::Mat> hsv_planes;
+    cv::split(HSV, hsv_planes);
+    cv::Mat h = hsv_planes[0];   // H channel
+    cv::Mat s = hsv_planes[1];   // S channel
+    cv::Mat v = hsv_planes[2];   // V channel
+
+    //// red color range
+    cv::Scalar hsv_l(1, 40, 133);
+    cv::Scalar hsv_h(90, 150, 255);
+    cv::Mat bw;
+    cv::inRange(HSV, hsv_l, hsv_h, bw);
+    // imshow("Specific Colour", bw);
+
+    return bw;
+}
--- include/LaneDetector.hpp
+++ include/LaneDetector.hpp
@@ -1,57 +1,37 @@
 #pragma once
-
+#include <iostream>
 #include <opencv2/highgui/highgui.hpp>
-#include<iostream>
 #include <string>
 #include <vector>
 #include "opencv2/opencv.hpp"
 
 class LaneDetector {
  private:
- 	 cv::Mat inputImage;
- 	 cv::Mat roiImage;
- 	 cv::Mat grayImage;
- 	 cv::Mat bwImage;
- 	 cv::Mat edgedImage;
- 	 cv::Mat threshImage;
- 	 std::vector<int> lines;
- 	 double rho = 1;
- 	 double theta = 1;
- 	 double hthreshold;
- 	 double leftSlope;
- 	 double rightSlope;
- 	 double leftPoints;
- 	 double rightPoints;
- 	 double meanLeftSlope;
- 	 double meanRightSlope;
- 	 string turnPredict;
+    cv::Mat inputImage;
+    cv::Mat roiImage;
+    cv::Mat grayImage;
+    cv::Mat bwImage;
+    cv::Mat edgedImage;
+    cv::Mat threshImage;
+    std::vector<int> lines;
+    double rho = 1;
+    double theta = 1;
+    double hthreshold;
+    double leftSlope;
+    double rightSlope;
+    double leftPoints;
+    double rightPoints;
+    double meanLeftSlope;
+    double meanRightSlope;
+    std::string turnPredict;
 
- public: 
-	 cv::Mat roiMastSelection(cv::Mat inputImage) {
-
-	 }
-
-	 cv::Mat hsvThreshold(cv::Mat roiImage) {
-
-	 }
-
-	 cv::Mat edgeDetector(cv::Mat threshImage) {
-
-	 }
-
-	 std::vector houghTransform(cv::Mat edgedImage) {
-
-	 }
-
-	 double slopeCalculation(std::vector<int>) {
-
-	 }
-
-	 string predictTurn(double, double){
-
-	 } 
-
-	 void plotResults(cv::Mat, string, std::vector<int>, std::vector<int>) {
-
-	 }
-}
+ public:
+    cv::Mat readFrame(int frameNumber);
+    cv::Mat roiMastSelection(cv::Mat inputImage);
+    cv::Mat hsvThreshold(cv::Mat roiImage);
+    cv::Mat edgeDetector(cv::Mat threshImage);
+    std::vector<int> houghTransform(cv::Mat edgedImage);
+    double slopeCalculation(std::vector<int>);
+    std::string predictTurn(double, double);
+    void plotResults(cv::Mat, std::string, std::vector<int>, std::vector<int>);
+};
