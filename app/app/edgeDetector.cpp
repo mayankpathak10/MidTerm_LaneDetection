d01736981c10e1d33e61f637c0d22bf43058bee1
bhargav-umd <bhargav@umd.edu>
2018-10-14 18:38:15 -0400

    SIP Task #23.1 Subtask Update Hough Transform and cleared unnecessary comments

--- app/app/edgeDetector.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/LaneDetector.hpp"
-
-cv::Mat LaneDetector::edgeDetector(cv::Mat morphImage) {
-    cv::Mat edge, draw;
-    Canny(morphImage, edge, 0, 255, 3);
-    edge.convertTo(draw, CV_8U);
-
-    return draw;
-}
