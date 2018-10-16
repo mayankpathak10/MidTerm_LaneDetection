4ed626c56c0428b0889ab84c1d669b096c1acbc4
Mayank <pathak10@umd.edu>
2018-10-15 20:07:12 -0400

    SIP Task#30.2 Comment hsvThresholdY.cpp

--- app/hsvThresholdY.cpp
+++ app/hsvThresholdY.cpp
@@ -1,21 +1,41 @@
+/**
+ * @file    hsvThresholdY.cpp
+ * @authors  Mayank Pathak and Bhargav Dandamudi
+ * @version 1.0
+ * @copyright (c) MIT License 2018 Mayank Pathak, Bhargav Dandamudi
+ *
+ * @brief Mid-Term Project (with partner component)
+ *
+ * @section DESCRIPTION
+ *
+ *  Split BGR image into HSV planes and apply threshold
+ *  on each plane to detect Yellow Lanes.
+ *
+ * @dependencies: This file depends on LaneDetector.hpp
+ */
+
 #include "../include/LaneDetector.hpp"
 
-cv::Mat LanePredictor::hsvThresholdY(cv::Mat frameP) {
-    cv::Mat HSV;
-
-    // convert RGB roiImage to HSV
-    cv::cvtColor(frameP, HSV, CV_BGR2HSV);
+/**
+ * LanePredictor::hsvThresholdY
+ * @param  frame [cv::Mat]
+ * @return threshold_image [cv::Mat]
+ */
+cv::Mat LanePredictor::hsvThresholdY(cv::Mat frame) {
+    cv::Mat hsv, threshold_image;
+    cv::Scalar hsv_low_y(1, 40, 133);
+    cv::Scalar hsv_high_y(90, 150, 255);
+    // convert RGB roiImage to hsv
+    cv::cvtColor(frame, hsv, CV_BGR2HSV);
     std::vector<cv::Mat> hsv_planes;
-    cv::split(HSV, hsv_planes);
+    cv::split(hsv, hsv_planes);
     cv::Mat h = hsv_planes[0];   // H channel
     cv::Mat s = hsv_planes[1];   // S channel
     cv::Mat v = hsv_planes[2];   // V channel
 
-    //// yellow color range
-    cv::Scalar hsv_ly(1, 40, 133);
-    cv::Scalar hsv_hy(90, 150, 255);
-    cv::Mat bw1;
-    cv::inRange(HSV, hsv_ly, hsv_hy, bw1);
+    //// yellow color range, found by trial and error
 
-    return bw1;
+    cv::inRange(hsv, hsv_low_y, hsv_high_y, threshold_image);
+
+    return threshold_image;
 }
